4eb4c4dbec69a6c4336463802e14570207b5f435
Filipe Morelli <morelitecinfo@gmail.com>
2017-04-03 16:30:13 -0300

    estrutura da tabela vaga_empresa

--- sql.sql
+++ sql.sql
@@ -58,4 +58,23 @@ CREATE TABLE `enderecos` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB;
 
-
+CREATE TABLE IF NOT EXISTS `vagas_empresa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `url_imagem` text NOT NULL,
+  `descricao_rapida` varchar(255) NOT NULL,
+  `site` text NOT NULL,
+  `periodo_trabalho` varchar(50) NOT NULL,
+  `salario` decimal(10,2) NOT NULL,
+  `horario_trabalho` varchar(50) NOT NULL,
+  `experiencia` varchar(50) NOT NULL,
+  `escolaridade` varchar(50) NOT NULL,
+  `descricao_completa` text NOT NULL,
+  `empresa_id` bigint(20) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `FK_vagas_empresa_empresas` (`empresa_id`),
+  CONSTRAINT `FK_vagas_empresa_empresas` FOREIGN KEY (`empresa_id`) REFERENCES `empresas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
