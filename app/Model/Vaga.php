55028f58106b68604f9e25e817c860afbd503a66
Filipe Morelli <morelitecinfo@gmail.com>
2017-04-05 18:33:10 -0300

    Add vaga validade no servidor

--- app/Model/Vaga.php
+++ app/Model/Vaga.php
@@ -29,12 +29,6 @@ class Vaga extends AppModel {
                 'message' => 'Digite um nome para o usuário'
             )
         ),
-        'ramo' => array(
-            'notEmpty' => array(
-                'rule' => array('notBlank'),
-                'message' => 'Digite o ramo da empresa!'
-            ),
-        ),
         'descricao_rapida' => array(
             'regra1' => array(
                 'rule' => array('notBlank'),
@@ -49,20 +43,6 @@ class Vaga extends AppModel {
                 'message' => 'Digite uma descrição com menos de 256 caracteres!'
             )
         ),
-        'qtde_empregados' => array(
-            'valid' => array(
-                'rule' => array('inList', array(
-                    'abaixo de 10',
-                    'abaixo de 50',
-                    'abaixo de 200',
-                    'abaixo de 500',
-                    'abaixo de 1000',
-                    'acima de 1001'
-                )),
-                'message' => 'Selecione a quantidade de funcionários',
-                'allowEmpty' => false
-            )
-        ),
         'site' => array(
             'notEmpty' => array(
                 'rule' => array('url'),
@@ -70,75 +50,58 @@ class Vaga extends AppModel {
                 'allowEmpty' => true,
             ),
         ),
-        'aniversario_empresa' => array(
-            'notEmpty' => array(
-                'rule' => array('date'),
-                'allowEmpty' => true,
-                'message' => 'Digite a data de anivesário da empresa!',
-            ),
-        ),
-        'telefone' => array(
-            'regra1' => array(
-                'rule' => array('notBlank'),
-                'message' => 'Digite o telefone de contato!',
-            ),
-            'regra2' => array(
-                'rule' => array('naturalNumber', true),
-                'message' => 'Digite somente numeros no telefone!',
+        'periodo_trabalho' => array(
+            'valid' => array(
+                'rule' => array('inList', array(
+                    'integral',
+                    'meio-horário',
+                    'diaria',
+                    'estagio',
+                    'frelancer',
+                    'remoto',
+                    'outro'
+                )),
+                'message' => 'Selecione a quantidade de funcionários',
             )
         ),
-        'email' => array(
-            'notEmpty' => array(
-                'rule' => array('email'),
-                'message' => 'Digite um email valido para o usuário!'
-            ),
+        'salario' => array(
+            'rule' => array('money', 'left'),
+            'message' => 'Digite o site salário do cargo.',
+            'allowEmpty' => true,
+        ),
+        'horario_trabalho' => array(
             'required' => array(
-                'rule' => array('isUnique'),
-                'message' => 'E-mail já cadastrado!',
-                'on' => 'create'
-            ),
+                'rule' => array('notBlank'),
+                'message' => 'Digite o horário de trabalho'
+            )
         ),
-        'url_facebook' => array(
-            'regra' => array(
-                'rule' => array('url'),
-                'message' => 'Digite a url da página ou perfil da sua empresa no facebook!',
-                'allowEmpty' => true,
-            ),
+        'experiencia' => array(
+            'required' => array(
+                'rule' => array('notBlank'),
+                'message' => 'Digite o experiência de trabalho'
+            )
         ),
-        'url_google_plus' => array(
-            'regra' => array(
-                'rule' => array('url'),
-                'message' => 'Digite a url da página ou perfil da sua empresa no Google +!',
-                'allowEmpty' => true,
-            ),
-        ),
-        'url_twitter' => array(
-            'regra' => array(
-                'rule' => array('url'),
-                'message' => 'Digite a url da perfil da sua empresa no Twitter!',
-                'allowEmpty' => true,
-            ),
-        ),
-        'url_instagram' => array(
-            'regra' => array(
-                'rule' => array('url'),
-                'message' => 'Digite a url do perfil da sua empresa no Instagram!',
-                'allowEmpty' => true,
-            ),
-        ),
-        'url_pinterest' => array(
-            'regra' => array(
-                'rule' => array('url'),
-                'message' => 'Digite a url da página ou perfil da sua empresa no Pinterest!',
-                'allowEmpty' => true,
-            ),
-        ),
-        'url_youtube' => array(
-            'regra' => array(
-                'rule' => array('url'),
-                'message' => 'Digite a url da página da sua empresa no Youtube!',
-                'allowEmpty' => true,
-            ),
+        'escolaridade' => array(
+            'valid' => array(
+                'rule' => array('inList', array(
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7',
+                    '8',
+                    '9',
+                    '10',
+                    '11',
+                    '12',
+                    '13',
+                    '14',
+                    '15',
+                )),
+                'message' => 'Selecione a quantidade de funcionários',
+            )
         ),
         'descricao_completa' => array(
             'regra1' => array(
