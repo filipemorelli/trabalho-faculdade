df4b37fe82cfd594a77f8d29379f48832e67d573
Filipe Morelli <morellitecinfo@gmail.com>
2017-09-20 20:33:03 -0300

    Filtrando porém não testado

--- app/Controller/EmpresasController.php
+++ app/Controller/EmpresasController.php
@@ -253,7 +253,9 @@ class EmpresasController extends AppController
             $this->request->data = $this->Session->read('EmpresaPesquisaVaga');
         }
 
-        //$conditions = $this->paginationConditionQuery($this->request->data);
+        $conditions = $this->paginationConditionQuery($this->request->data);
+
+        $conditions['Empresa.user_id'] = $this->Session->read('Auth.User.id');
 
         $this->Paginator->settings = array(
             'fields'     => array(
@@ -267,9 +269,7 @@ class EmpresasController extends AppController
                 'Endereco.cidade',
                 'Endereco.estado'
             ),
-            'conditions' => array(
-                'Empresa.user_id' => $this->Session->read('Auth.User.id')
-            ),
+            'conditions' => $conditions,
             'order'      => array(
                 'Vaga.modified' => 'DESC',
                 'Vaga.id'       => 'DESC'
@@ -384,6 +384,127 @@ class EmpresasController extends AppController
 
         $this->set('vaga', $vaga);
     }
+
+        /**
+     * Manipula as condições da paginação de pesquisa de vagas
+     *
+     * @param array $data
+     * @return array
+     */
+     private function paginationConditionQuery($data)
+     {
+ 
+         $conditions = array();
+ 
+         if (isset($data['Users']['cargo']) && strlen($data['Users']['cargo']) > 0) {
+             $nomeVaga = $data['Users']['cargo'];
+             $conditions['OR'] = array(
+                 'Vaga.nome LIKE'                  => "%$nomeVaga%",
+                 'Vaga.descricao_rapida LIKE'      => "%$nomeVaga%",
+                 'Vaga.descricao_completa LIKE'    => "%$nomeVaga%",
+                 'Empresa.nome LIKE'               => "%$nomeVaga%",
+                 'Empresa.descricao_rapida LIKE'   => "%$nomeVaga%",
+                 'Empresa.descricao_completa LIKE' => "%$nomeVaga%"
+             );
+         }
+ 
+         if (isset($data['Users']['cidade']) && strlen($data['Users']['cidade']) > 0) {
+             $conditions['Endereco.cidade'] = $data['Users']['cidade'];
+         }
+ 
+         if (isset($data['Users']['estado']) && strlen($data['Users']['estado']) > 0) {
+             $conditions['Endereco.estado'] = $data['Users']['estado'];
+         }
+ 
+         if (isset($data['Users']['salario']) && $data['Users']['estado'] >= 0) {
+             $salarioOperador = isset($data['Users']['salario-operador']) ? $data['Users']['salario-operador'] : '>=';
+             $conditions["Vaga.salario $salarioOperador"] = $data['Users']['salario'];
+         }
+ 
+         if (isset($data['Users']['escolaridade']) && $data['Users']['escolaridade'] >= 0) {
+             $conditions['Vaga.escolaridade >='] = $data['Users']['escolaridade'];
+         }
+ 
+         if (isset($data['Users']['periodo_trabalho']) && strlen($data['Users']['periodo_trabalho']) > 0) {
+             $conditions['Vaga.periodo_trabalho'] = $data['Users']['periodo_trabalho'];
+         }
+ 
+         if (isset($data['Users']['horas']) && is_array($data['Users']['horas'])) {
+             $conditions['Vaga.horario_trabalho'] = $data['Users']['horas'];
+         }
+ 
+         if (isset($data['Users']['tempo_vaga']) && strlen($data['Users']['tempo_vaga'])) {
+             switch ($data['Users']['tempo_vaga']) {
+                 case 'hoje':
+                     $conditions['Vaga.modified = '] = CakeTime::format(time(), '%Y-%m-%d');
+                     break;
+                 case '3 dias':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('-3 days', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '1 semana':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('1 week ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '2 semanas':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('2 weeks ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '3 semanas':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('3 weeks ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '1 mes':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('1 month ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '2 meses':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('2 month ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '3 meses':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('3 month ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+                 case '6 meses':
+                     $conditions['Vaga.modified BETWEEN ? and ?'] = array(
+                         CakeTime::format('6 month ago', '%Y-%m-%d'),
+                         CakeTime::format(time(), '%Y-%m-%d')
+                     );
+                     //$conditions['Vaga.modi1fied'] = 'BETWEEN now() AND now() - INTERVAL 3 DAY';
+                     break;
+ 
+                 default:
+                     # code...
+                     break;
+             }
+             //$conditions['Vaga.horario_trabalho'] = '';
+         }
+ 
+         return $conditions;
+     }
+ 
 
     /**
      * Mostra candidatos à vaga
